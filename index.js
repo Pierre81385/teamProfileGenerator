d07d0e484a2bbce3152eff3917b76c534cffd06a
Peter Bishop <pjb.den@gmail.com>
2021-05-17 04:49:49 -0600

    added working prompts for populating employee properties

--- index.js
+++ index.js
@@ -1,18 +1,167 @@
-const { INTERNAL_MATCHER_FLAG } = require("expect/build/jestMatchersObject");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const Employee = require("./lib/classes");
-const Engineer = require("./lib/classes");
-const Manager = require("./lib/classes");
-const Intern = require("./lib/classes");
 
-function init() {
-  const newEmployee = new Employee();
-  const newEngineer = new Engineer();
-  const newManager = new Manager();
-  const newIntern = new Intern();
-
-  console.log("Please build your team.");
-  newEmployee.getRole();
+function managerPrompts() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the manager's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email address?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's office number?",
+    },
+    {
+      type: "list",
+      name: "addEmployee",
+      message: "Would you like to add an engineer or intern?",
+      choices: ["Engineer", "Intern", "None"],
+    },
+  ]);
 }
 
-init();
+function engineerPrompt() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the engineer's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the engineer's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the engineer's email address?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is the engineer's GitHub name?",
+    },
+    {
+      type: "list",
+      name: "addEmployee",
+      message: "Would you like to add another engineer or intern?",
+      choices: ["Engineer", "Intern", "None"],
+    },
+  ]);
+}
+
+function internPrompt() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's ID number?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email address?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What school did the intern come from?",
+    },
+    {
+      type: "list",
+      name: "addEmployee",
+      message: "Would you like to add another engineer or intern?",
+      choices: ["Engineer", "Intern", "None"],
+    },
+  ]);
+}
+
+const allEmployee = [];
+
+function makeEngineer() {
+  engineerPrompt().then(function (response) {
+    const engineer = new Engineer(
+      response.name,
+      response.id,
+      response.email,
+      response.github
+    );
+    if (response.addEmployee == "Engineer") {
+      allEmployee.push(engineer);
+      makeEngineer();
+    } else if (response.addEmployee == "Intern") {
+      allEmployee.push(engineer);
+      makeIntern();
+    } else if (response.addEmployee == "None") {
+      allEmployee.push(engineer);
+      return;
+    }
+  });
+}
+
+function makeIntern() {
+  internPrompt().then(function (response) {
+    const intern = new Intern(
+      response.name,
+      response.id,
+      response.email,
+      response.school
+    );
+    if (response.addEmployee == "Engineer") {
+      allEmployee.push(intern);
+      makeEngineer();
+    } else if (response.addEmployee == "Intern") {
+      allEmployee.push(intern);
+      makeIntern();
+    } else if (response.addEmployee == "None") {
+      allEmployee.push(intern);
+      return;
+    }
+  });
+}
+
+function makeManager() {
+  managerPrompts().then(function (response) {
+    const manager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.officeNumber
+    );
+
+    if (response.addEmployee == "Engineer") {
+      allEmployee.push(manager);
+      makeEngineer();
+    } else if (response.addEmployee == "Intern") {
+      allEmployee.push(manager);
+      makeIntern();
+    } else if (response.addEmployee == "None") {
+      allEmployee.push(manager);
+
+      return;
+    }
+  });
+}
+
+console.log("Please build your team.");
+makeManager();
