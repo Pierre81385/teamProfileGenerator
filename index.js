8c0d56bd6a1b2aa4375d85de7f4a6c9e79a12f28
Peter Bishop <pjb.den@gmail.com>
2021-05-17 04:31:33 -0600

    added new class files for each employee type

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const { INTERNAL_MATCHER_FLAG } = require("expect/build/jestMatchersObject");
 const inquirer = require("inquirer");
-const Employee = require("../lib/classes");
+const Employee = require("./lib/classes");
+const Engineer = require("./lib/classes");
+const Manager = require("./lib/classes");
+const Intern = require("./lib/classes");
 
-var allEmployees = [];
+function init() {
+  const newEmployee = new Employee();
+  const newEngineer = new Engineer();
+  const newManager = new Manager();
+  const newIntern = new Intern();
 
-const newEmployee = new Employee();
+  console.log("Please build your team.");
+  newEmployee.getRole();
+}
 
-newEmployee.getName();
+init();
