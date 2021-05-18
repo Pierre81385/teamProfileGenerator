9c1d4e5bf27179b0cbe31500773704b2f9bc1eed
Peter Bishop <pjb.den@gmail.com>
2021-05-17 23:37:35 -0600

    created first successful test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,15 @@
-const Employee = require("../lib/classes");
+const Employee = require("../lib/Employee");
 
-describe("employeeClass", () => {
-  it("When I initilize the class", () => {
-    const obj = new Employee("Peter", 9999, "pjb.den@gmail.com");
-    expect;
+describe("Employee Class", () => {
+  it("When I initilize the class, a new Employee object is created.", () => {
+    const testID = "0000";
+    const testName = "test";
+    const testEmail = "test@email.com";
+
+    const testEmployee = new Employee(testName, testID, testEmail);
+
+    expect(testEmployee.id).toEqual(testID);
+    expect(testEmployee.name).toEqual(testName);
+    expect(testEmployee.email).toEqual(testEmail);
   });
 });
